55460023e8f1200d329e698196d49f48a12194c0
gitpredrags <gitpredrags@gmail.com>
2023-01-16 16:05:45 +0100

    mali update UpdateUserRequest-a

--- api-client/src/main/java/com/projectname/api/client/data/model/users/update/UpdateUserRequest.java
+++ api-client/src/main/java/com/projectname/api/client/data/model/users/update/UpdateUserRequest.java
@@ -1,17 +1,43 @@
 package com.projectname.api.client.data.model.users.update;
 
-import com.projectname.api.client.data.model.users.create.CreateUserRequest;
-
 import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
-public class UpdateUserRequest extends CreateUserRequest implements Serializable {
+public class UpdateUserRequest implements Serializable
+{
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("job")
+    @Expose
+    private String job;
+    private final static long serialVersionUID = 5357147195295600320L;
 
     public UpdateUserRequest() {
-        super();
     }
 
     public UpdateUserRequest(String name, String job) {
-        super(name, job);
+        super();
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
     }
 
 }
